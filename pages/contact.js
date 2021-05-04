4e277952d51b0e82d392b331edf0e90d540ab17a
Daniel <dkburton0@gmail.com>
2021-05-04 12:10:15 +0100

    Added icons to the text in the contact page

--- pages/contact.js
+++ pages/contact.js
@@ -109,37 +109,73 @@ export default function Contact() {
       {/* Start of Container */}
       <div className="container">
         {/* Section */}
-        <div className="row my-5 justify-content-evenly py-5">
+        <div className="row my-5 justify-content-evenly align-items-center py-5">
           <div className="col-md-6" data-aos="fade-up">
             <ul className="mb-5">
-              <li>
-                <h4 className="mb-3">SESIÓN ONLINE</h4>
-                <p>¿Cómo funciona?</p>
-              </li>
-              <li>
-                <h4 className="mb-3">RESERVA TU CITA</h4>
-                <p>
-                  A través de la web, por email: info@marisolcorona.com o al
-                  teléfono +7539390244.
-                </p>
-              </li>
-              <li>
-                <h4 className="mb-3">PAGA ONLINE</h4>
-                <p>
-                  A través de nuestra web con nuestra pasarela de pago segura.
-                </p>
-              </li>
-              <li>
-                <h4 className="mb-3">CONFIRMACIÓN </h4>
-                <p>
-                  Recibirás un correo electrónico con los pasos a seguir para la
-                  sesión online.
-                </p>
-              </li>
-              <li>
-                <h4 className="mb-3">SKYPE O ZOOM</h4>
-                <p>Añade esta cuenta a tu Skype: corona.marisol_1</p>
-              </li>
+              <div className="row align-items-center">
+                <div className="col-sm-1">
+                  <i className="fas fa-info fa-2x"></i>
+                </div>
+                <div className="col-sm-10">
+                  <li>
+                    <h4 className="mb-3">SESIÓN ONLINE</h4>
+                    <p>¿Cómo funciona?</p>
+                  </li>
+                </div>
+              </div>
+              <div className="row align-items-center">
+                <div className="col-sm-1">
+                  <i className="fas fa-info fa-2x"></i>
+                </div>
+                <div className="col-sm-10">
+                  <li>
+                    <h4 className="mb-3">RESERVA TU CITA</h4>
+                    <p>
+                      A través de la web, por email: info@marisolcorona.com o al
+                      teléfono +7539390244.
+                    </p>
+                  </li>
+                </div>
+              </div>
+              <div className="row align-items-center">
+                <div className="col-sm-1">
+                  <i className="fas fa-info fa-2x"></i>
+                </div>
+                <div className="col-sm-10">
+                  <li>
+                    <h4 className="mb-3">PAGA ONLINE</h4>
+                    <p>
+                      A través de nuestra web con nuestra pasarela de pago
+                      segura.
+                    </p>
+                  </li>
+                </div>
+              </div>
+              <div className="row align-items-center">
+                <div className="col-sm-1">
+                  <i className="fas fa-info fa-2x"></i>
+                </div>
+                <div className="col-sm-10">
+                  <li>
+                    <h4 className="mb-3">CONFIRMACIÓN </h4>
+                    <p>
+                      Recibirás un correo electrónico con los pasos a seguir
+                      para la sesión online.
+                    </p>
+                  </li>
+                </div>
+              </div>
+              <div className="row align-items-center">
+                <div className="col-sm-1">
+                  <i className="fas fa-info fa-2x"></i>
+                </div>
+                <div className="col-sm-10">
+                  <li>
+                    <h4 className="mb-3">SKYPE O ZOOM</h4>
+                    <p>Añade esta cuenta a tu Skype: corona.marisol_1</p>
+                  </li>
+                </div>
+              </div>
             </ul>
           </div>
           <div className="col-md-4" data-aos="fade-up">
@@ -235,67 +271,6 @@ export default function Contact() {
                 </Formik>
               }
             </div>
-            {/* <form>
-              <h2 className="text-center mb-4">Escríbenos</h2>
-              <div className="form-outline mb-4">
-                <label className="form-label" htmlFor="form4Example1">
-                  <i className="fas fa-user me-2"></i>
-                  Tu nombre
-                </label>
-                <input
-                  type="text"
-                  id="form4Example1"
-                  className="form-control"
-                />
-              </div>
-
-              <div className="form-outline mb-4">
-                <label className="form-label" htmlFor="form4Example2">
-                  <i className="fas fa-envelope prefix grey-text me-2"></i>
-                  Tu correo electrónico
-                </label>
-                <input
-                  type="email"
-                  id="form4Example2"
-                  className="form-control"
-                />
-              </div>
-
-              <div className="form-outline mb-4">
-                <label className="form-label" htmlFor="form4Example3">
-                  <i className="fas fa-envelope me-2"></i>
-                  Tu mensaje
-                </label>
-                <textarea
-                  className="form-control"
-                  id="form4Example3"
-                  rows="4"
-                ></textarea>
-              </div>
-
-              <div className="form-check d-flex justify-content-center mb-4">
-                <input
-                  className="form-check-input me-2"
-                  type="checkbox"
-                  value=""
-                  id="form4Example4"
-                />
-                <label className="form-check-label" htmlFor="form4Example4">
-                  Suscríbeme al boletín
-                </label>
-              </div>
-              <div className="row justify-content-center">
-                <button
-                  id={styles.headerButtonPink}
-                  className="btn mx-2 mt-4 py-3 px-5 text-white"
-                  href=""
-                  role="button"
-                  type="submit"
-                >
-                  Send
-                </button>
-              </div>
-            </form> */}
           </div>
         </div>
       </div>
