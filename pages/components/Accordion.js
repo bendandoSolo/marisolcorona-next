22a6e66f5f6f07a6b101ba4c293438643e7837b1
Daniel <dkburton0@gmail.com>
2021-05-06 16:46:35 +0100

    Changed to yarn

--- pages/components/Accordion.js
+++ pages/components/Accordion.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Accordian = () => {
   return (
-    <div class="accordion accordion-flush" id="accordionFlushExample">
-      <div class="accordion-item">
-        <h2 class="accordion-header" id="flush-headingOne">
+    <div className="accordion accordion-flush" id="accordionFlushExample">
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="flush-headingOne">
           <button
-            class="accordion-button collapsed"
+            className="accordion-button collapsed"
             type="button"
             data-mdb-toggle="collapse"
             data-mdb-target="#flush-collapseOne"
@@ -18,11 +18,11 @@ const Accordian = () => {
         </h2>
         <div
           id="flush-collapseOne"
-          class="accordion-collapse collapse"
+          className="accordion-collapse collapse"
           aria-labelledby="flush-headingOne"
           data-mdb-parent="#accordionFlushExample"
         >
-          <div class="accordion-body">
+          <div className="accordion-body">
             <p>¿Acabas de terminar una relación?</p>
             <p>
               ¿Piensas que la vida no tiene sentido sin una pareja creando una
@@ -52,10 +52,10 @@ const Accordian = () => {
           </div>
         </div>
       </div>
-      <div class="accordion-item">
-        <h2 class="accordion-header" id="flush-headingTwo">
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="flush-headingTwo">
           <button
-            class="accordion-button collapsed"
+            className="accordion-button collapsed"
             type="button"
             data-mdb-toggle="collapse"
             data-mdb-target="#flush-collapseTwo"
@@ -67,11 +67,11 @@ const Accordian = () => {
         </h2>
         <div
           id="flush-collapseTwo"
-          class="accordion-collapse collapse"
+          className="accordion-collapse collapse"
           aria-labelledby="flush-headingTwo"
           data-mdb-parent="#accordionFlushExample"
         >
-          <div class="accordion-body">
+          <div className="accordion-body">
             <p>
               <strong>Probablemente nadie te ha enseñado a...</strong>
             </p>
@@ -88,7 +88,7 @@ const Accordian = () => {
               Crear relaciones de pareja más sanas que te harán sentir mejor y
               completa.
             </p>
-            <p className="full-width"> ¡Encontrar a tu pareja ideal!</p>
+            <p classNameName="full-width"> ¡Encontrar a tu pareja ideal!</p>
             <p>
               <strong>¿Te gustaría poder lograr todo esto y más?</strong>
             </p>
