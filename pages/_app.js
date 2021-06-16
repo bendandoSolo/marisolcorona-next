77d1d2097d8869a53f3fb9b1030a1015fdb379f3
Daniel <dkburton0@gmail.com>
2021-06-16 10:09:42 +0100

    Installed react-fb-pixel

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,24 @@
 import { useEffect } from "react";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
 import "../styles/globals.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import * as gtag from '../lib/gtag';
+import * as gtag from "../lib/gtag";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { AnimatePresence } from "framer-motion";
 
-
 function FacebookPixel() {
-  const nextRouter = useRouter()
+  const nextRouter = useRouter();
 
   useEffect(() => {
     import("react-facebook-pixel")
       .then((x) => x.default)
       .then((ReactPixel) => {
-        ReactPixel.init('2576191972631019');
+        ReactPixel.init("2576191972631019");
         ReactPixel.pageView();
 
         nextRouter.events.on("routeChangeComplete", () => {
@@ -30,38 +29,35 @@ function FacebookPixel() {
   return null;
 }
 
-
-
-
 const MyApp = ({ Component, pageProps, router }) => {
-  const nextRouter = useRouter()
+  const nextRouter = useRouter();
 
   useEffect(() => {
     const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
+      gtag.pageview(url);
+    };
     //unimplement analytics boilerplate code
-    nextRouter.events.on('routeChangeComplete', handleRouteChange)
+    nextRouter.events.on("routeChangeComplete", handleRouteChange);
     return () => {
-      nextRouter.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [nextRouter.events])
-
+      nextRouter.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [nextRouter.events]);
 
   useEffect(() => {
     AOS.init({ once: true });
   });
 
-return(
-  <>
-      <Header/>
+  return (
+    <>
+      <Header />
       <FacebookPixel />
       <Navbar />
       <AnimatePresence exitBeforeEnter>
-           <Component {...pageProps} key={router.route} />
-         </AnimatePresence>
+        <Component {...pageProps} key={router.route} />
+      </AnimatePresence>
       <Footer />
-  </>
-)};
+    </>
+  );
+};
 
 export default MyApp;
